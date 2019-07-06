916c9d6bb1f78844e30d7133bd1e1840f958fe05
Pekes317 <El_Dante565@yahoo.com>
2019-07-05 21:52:55 -0500

    added auto build scripts

--- tools/post-build.js
+++ tools/post-build.js
@@ -1,1 +1,9 @@
 const shell = require('shelljs');
+
+shell.cp('tools/package.json', 'dist/package.json');
+shell.mkdir('public');
+shell.cp('-r', 'dist/views', 'public');
+shell.mv('public/views/*', 'public');
+shell.mv('public/index.html', 'public/index2.html');
+shell.rm('-r', 'public/views');
+shell.rm('-r', 'dist/views/assets');
